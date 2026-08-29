d8b4e6323e93c6b64b30f2f5f0a263144627e758
VinitAaeer <aaeervinit@gmail.com>
2026-08-29 16:45:18 +0530

    Implement customer retention analysis queries
    Added SQL queries to analyze customer dormancy and reactivation.

--- sql/07_customer_retention.sql
+++ sql/07_customer_retention.sql
@@ -1,1 +1,40 @@
+/*
+ 5. Customer Dormancy & Reactivation Analysis
+	Problem:** Track dormant accounts (no activity for 180 days) and re-activated customer behavior.
+	SQL Queries Included:** Q41, Q42
+	Business Impact:** Drives targeted marketing campaigns to retain at-risk customers.
+*/
+
+-- — CUSTOMER RETENTION
+-- Q12. Find customers who haven't transacted for 180 days.
+SELECT
+    customer_id,
+    MAX(transaction_date) AS last_transaction
+FROM transactions
+WHERE status = 'Success'
+GROUP BY customer_id
+HAVING last_transaction <
+       CURRENT_DATE - INTERVAL 180 DAY;
+       
+-- Q13. Find customers who became active again after 90 days.
+WITH x AS (
+    SELECT
+        customer_id,
+        transaction_date,
+
+        LAG(transaction_date) OVER (
+            PARTITION BY customer_id
+            ORDER BY transaction_date
+        ) AS previous_transaction
+
+    FROM transactions
+
+    WHERE status = 'Success'
+)
+SELECT *
+FROM x
+WHERE DATEDIFF(
+          transaction_date,
+          previous_transaction
+      ) >= 90;
 
