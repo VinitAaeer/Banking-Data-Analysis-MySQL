7def33b7c50b4b07f37a4e70795741329c06d91f
VinitAaeer <aaeervinit@gmail.com>
2026-08-29 16:45:49 +0530

    Implement Pareto analysis SQL query
    Added SQL query for Pareto analysis to identify top 20% of customers contributing to 80% of transaction volume.

--- sql/08_pareto_analysis.sql
+++ sql/08_pareto_analysis.sql
@@ -1,1 +1,33 @@
+/*
+  6. Pareto Analysis (80/20 Revenue Rule)
+	Problem:** Identify the top 20% of customer cohorts driving 80% of total transaction volume.
+	SQL Queries Included:** Q55
+	Business Impact:** Focuses retention efforts on the most profitable bank accounts.
+*/
+
+-- Q.14  Find customers contributing to first 80% of transaction value.
+WITH customer_value AS (
+       SELECT
+            customer_id, SUM(amount) AS value
+       FROM transactions
+       WHERE status = 'Success'
+       GROUP BY customer_id),
+x AS (
+    SELECT  *,SUM(value) OVER (ORDER BY value DESC) AS cumulative_value ,
+                      SUM(value) OVER () AS total_value
+    FROM customer_value
+        )
+SELECT
+	customer_id,    concat(round(value/100000,1),' L') value,
+     CASE
+        WHEN cumulative_value >= 10000000 THEN
+            CONCAT(ROUND(cumulative_value * 1.0 / 10000000, 2), ' Cr')
+        WHEN cumulative_value >= 100000 THEN
+            CONCAT(ROUND(cumulative_value * 1.0 / 100000, 2), ' L')
+        ELSE
+            CONCAT(ROUND(cumulative_value, 2), '')
+    END AS sales_formatted,
+    concat( round(100.0 *cumulative_value /   total_value ,2),' %') AS cumulative_percentage
+FROM x
+WHERE   cumulative_value / total_value <= 0.80;
 
